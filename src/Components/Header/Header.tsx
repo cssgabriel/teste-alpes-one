f1f57a057f7c4610d7478a378d51592c2ffff859
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2024-02-29 12:24:08 -0300

    feat: separando lista de botões em componente separado

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import ListButtons from "./ListButtons";
+
+const Header = () => {
+  return (
+    <header className="header d-flex gap-md flex-column">
+      <ListButtons />
+      <nav></nav>
+    </header>
+  );
+};
+
+export default Header;
--- src/Components/Header/ListButtons.tsx
+++ src/Components/Header/ListButtons.tsx
@@ -1,20 +1,24 @@
-import Circle360SVG from "../assets/icons/svg/Circle360SVG";
-import TelefoneSVG from "../assets/icons/svg/TelefoneSVG";
-import WhatsappSVG from "../assets/icons/svg/WhatsappSVG";
-import Button from "./Utils/Button";
+import Circle360SVG from "../../assets/icons/svg/Circle360SVG";
+import TelefoneSVG from "../../assets/icons/svg/TelefoneSVG";
+import WhatsappSVG from "../../assets/icons/svg/WhatsappSVG";
+import Button from "../Utils/Button";
 
-const Header = () => {
+const ListButtons = () => {
   return (
-    <header className="header">
-      <nav className="d-flex gap-md">
+    <ul className="d-flex gap-md">
+      <li>
         <Button href="tel:+552120424442">
           <TelefoneSVG />
           (21)2042-4442
         </Button>
+      </li>
+      <li>
         <Button style={{ textTransform: "uppercase" }}>
           <Circle360SVG />
           Tour virtual
         </Button>
+      </li>
+      <li>
         <Button
           style={{
             textTransform: "uppercase",
@@ -24,9 +28,9 @@ const Header = () => {
           <WhatsappSVG />
           Whatsapp
         </Button>
-      </nav>
-    </header>
+      </li>
+    </ul>
   );
 };
 
-export default Header;
+export default ListButtons;

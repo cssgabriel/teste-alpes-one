1c750c30c4f8cbfd35babbd701bfb2900a324b5e
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2024-02-29 17:18:59 -0300

    feat: form completo

--- src/Components/Utils/Form/Form.tsx
+++ src/Components/Utils/Form/Form.tsx
@@ -1,10 +1,41 @@
+import CarroSVG from "../../../assets/icons/svg/CarroSVG";
+import Button from "../Button";
 import "./Form.scss";
 import Input from "./Input";
 
 const Form = () => {
   return (
     <form className="form" onSubmit={(e) => e.preventDefault()}>
-      <Input htmlFor="Nome" id="name" />
+      <fieldset>
+        <legend className="d-flex">
+          <CarroSVG fill="var(--color-s)" /> Solicitar cotação
+        </legend>
+        <Input label={{ htmlFor: "Nome" }} input={{ id: "name" }} />
+        <Input
+          label={{ htmlFor: "Email" }}
+          input={{ id: "email", type: "email" }}
+        />
+        <Input
+          label={{ htmlFor: "Telefone" }}
+          input={{ id: "tel", type: "tel" }}
+        />
+        <Input label={{ htmlFor: "Modelo" }} input={{ id: "model" }} />
+        <Input label={{ htmlFor: "Mensagem" }} input={{ id: "message" }} />
+        <Input
+          label={{ htmlFor: "terms" }}
+          input={{ id: "terms", type: "checkbox" }}
+        >
+          <span>
+            Aceito a <strong>Política de Privacidade</strong>
+          </span>
+        </Input>
+        <Button
+          href="#"
+          style={{ backgroundColor: "var(--color-s)", textAlign: "center" }}
+        >
+          Enviar
+        </Button>
+      </fieldset>
     </form>
   );
 };
--- src/Components/Utils/Form/Input.tsx
+++ src/Components/Utils/Form/Input.tsx
@@ -1,21 +1,52 @@
 import React from "react";
 
 const Input = (
-  props: React.PropsWithChildren &
-    React.InputHTMLAttributes<HTMLInputElement> &
-    React.LabelHTMLAttributes<HTMLLabelElement>
+  props: React.PropsWithChildren & {
+    label: React.LabelHTMLAttributes<HTMLLabelElement>;
+    input: React.InputHTMLAttributes<HTMLInputElement>;
+  }
 ) => {
   const [value, setValue] = React.useState("");
+
+  if (props.input.type === "checkbox") {
+    return (
+      <>
+        <label
+          {...props.label}
+          style={{ display: "flex", gap: "10px", padding: "1.6rem 0" }}
+        >
+          <input
+            type="checkbox"
+            onChange={({ target }) => setValue(target.value)}
+            id={props.input.id}
+            name={props.input.name ?? props.input.id}
+            placeholder={value ? "" : props.label.htmlFor}
+            value={value}
+            {...props.input}
+          />
+          {props.children}
+        </label>
+      </>
+    );
+  }
+
   return (
-    <div>
-      <label htmlFor={props.htmlFor}>{props.htmlFor}</label>
+    <div className="position-relative">
+      <label
+        className={`position-absolute ${value ? "filled" : ""}`}
+        htmlFor={props.label.htmlFor}
+        {...props.label}
+      >
+        {props.label.htmlFor}
+      </label>
       <input
         onChange={({ target }) => setValue(target.value)}
-        type={props.type ?? "text"}
-        id={props.id}
-        name={props.name ?? props.id}
+        type={props.input.type ?? "text"}
+        id={props.input.id}
+        name={props.input.name ?? props.input.id}
+        placeholder={value ? "" : props.label.htmlFor}
         value={value}
-        {...props}
+        {...props.input}
       />
     </div>
   );
